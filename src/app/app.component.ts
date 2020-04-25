69de94e4cf195a9f2583b843396c5ae9dc69425a
David Allen <davetvmusic@hotmail.com>
2020-04-25 10:39:28 +1000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'form-array-new';
+
+  carsArr = new FormArray([]);
+
+  ngOnInit(): void {
+    this.addCar();
+  }
+  
+  // create the form, 
+  public myForm = new FormGroup({
+    name: new FormControl(''),
+    cars: this.carsArr
+  });
+
+  public addCar(): void {
+    const group = new FormGroup({
+      make: new FormControl(''),
+      colour: new FormControl('')
+    });
+
+    this.carsArr.push(group);
+  }
+
+  public deleteCar(index: number): void {
+    this.carsArr.removeAt(index);
+  }
+
+  public saveForm(): void {
+    console.log(this.myForm.value);
+    
+  }
 }
